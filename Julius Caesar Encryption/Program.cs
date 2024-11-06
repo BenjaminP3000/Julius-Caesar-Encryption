23b01b2a92cefe4cf7fa6e7a3be447ccfdc9045a
BenjaminP3000 <benjaminpersson@hotmail.se>
2024-11-06 10:44:52 +0100

    #

--- Julius Caesar Encryption/Program.cs
+++ Julius Caesar Encryption/Program.cs
@@ -1,31 +1,77 @@
-﻿namespace Julius_Caesar_Encryption
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Julius_Caesar_Encryption
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Menu();
         }
 
-        public void Menu()
+        public static void Menu()
         {
-            Console.WriteLine("1. Encrypt message");
-            Console.WriteLine("2. Decrypt message");
-            Console.WriteLine("3. Decrypt without key");
+
+            while (true)
+            {
+                Console.WriteLine("1. Encrypt message");
+                Console.WriteLine("2. Decrypt message");
+                Console.WriteLine("3. Decrypt without key");
+                string menuChoice = Console.ReadLine();
+
+                switch (menuChoice)
+                {
+                    case "1":
+                        Encrypt();
+                        break;
+                    case "2":
+                        Console.WriteLine();
+                        break;
+                    case "3":
+                        Console.WriteLine();
+                        break;
+                    default:
+                        Console.WriteLine();
+                        continue;
+                }
+            }
+
         }
 
-        public void StringToInt(string userString)
+        public static int[] StringToInt(string userString)
         {
+            char[] charArray = userString.ToCharArray();
+            int[] intArray = new int[charArray.Length];
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                intArray[i] = (int)charArray[i];
+            }
+            return intArray;
 
         }
 
-        public void IntToString(int[] encryptedIntArray)
+        public static void IntToString(int[] intArray)
         {
+            string encryptedString = intArray.ToString();
 
+            Console.WriteLine(encryptedString);
         }
 
-        public void Encrypt(int[] userIntArray)
+        public static void Encrypt()
         {
+            Console.WriteLine("Enter message to encrypt:");
+            string userMessage = Console.ReadLine().ToUpper();
+            Console.WriteLine("Enter encryption key (number 1 - 27): ");
+            int encryptionKey = Convert.ToInt32(Console.ReadLine());
+
+            int[] intArray = StringToInt(userMessage);
+
+            foreach (int i in intArray)
+            {
+                intArray[i] = intArray[i] + encryptionKey;
+            }
+
+
 
         }
 
