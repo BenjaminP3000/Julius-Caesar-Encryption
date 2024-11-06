f19f205561b07e6a9f7900a8d94b8a995c897a36
BenjaminP3000 <benjaminpersson@hotmail.se>
2024-11-06 09:54:35 +0100

    Add project files.

---
+++ Julius Caesar Encryption/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Julius_Caesar_Encryption
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("1. Encrypt message");
+            Console.WriteLine("2. Decrypt message");
+            Console.WriteLine("3. Decrypt without key");
+        }
+
+        public void StringToInt(string userString)
+        {
+
+        }
+
+        public void IntToString(int[] encryptedIntArray)
+        {
+
+        }
+
+        public void Encrypt(int[] userIntArray)
+        {
+
+        }
+
+    }
+}
