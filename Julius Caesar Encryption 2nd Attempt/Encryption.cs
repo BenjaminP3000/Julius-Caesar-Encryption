dd9aea7321870439bbfdbe603018b494539509bd
BenjaminP3000 <benjaminpersson@hotmail.se>
2024-11-07 09:37:11 +0100

    #

---
+++ Julius Caesar Encryption 2nd Attempt/Encryption.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Julius_Caesar_Encryption_2nd_Attempt
+{
+    internal class Encryption
+    {
+        public static void EncryptMessage()
+        {
+            Console.Write("Skriv meddelande för kryptering: ");
+            string userMessage = Console.ReadLine();
+            Console.Write("Ange krypteringsnyckel (siffra 1-26): ");
+            int encryptionKey = Convert.ToInt32(Console.ReadLine());
+
+            var charArray = new char[userMessage.Length];
+
+            for (int i = 0; i < userMessage.Length; i++)
+            {
+                charArray[i] = userMessage[i];
+            }
+
+
+        }
+    }
+}
---
+++ Julius Caesar Encryption 2nd Attempt/Menu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Julius_Caesar_Encryption_2nd_Attempt
+{
+    internal class Menu
+    {
+        public static void MainMenu()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Kryptera meddelande");
+                Console.WriteLine("2. Dekryptera meddelande");
+                Console.WriteLine("3. Dekryptera meddelande utan nyckel");
+
+                string menuChoice = Console.ReadLine();
+
+                switch (menuChoice)
+                {
+                    case "1":
+                        Encryption.EncryptMessage();
+                        break;
+
+                    case "2":
+                        // Kod för dekryptering
+                        break;
+
+                    case "3":
+                        // Kod för dekryptering utan nyckel
+                        break;
+
+                    default:
+                        // Kod för fel val
+                        continue;
+
+                }
+            }
+        }
+    }
+}
